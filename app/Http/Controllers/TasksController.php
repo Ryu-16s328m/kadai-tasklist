1419e64d6356c591cea819145fa800dbc566c131
Ryuutarou.Yamamoto <schuxinzhedesu@gmail.com>
2018-11-28 14:53:14 +0000

    Re:resubmit

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -94,7 +94,13 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+         $this->validate($request, [
+            'title' => 'required|max:10',
+            'content' => 'required|max:191',
+        ]);
+        
         $task = Task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
